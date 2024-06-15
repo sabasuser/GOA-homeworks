cd644ebabac20441011eca5a26abac17bfaedcb5
sabasuser <sabasuser@gmail.com>
2024-06-15 22:49:48 +0400

    commit

---
+++ level10/classwork.py
@@ -0,0 +1,11 @@
+num1 = int(10)
+num2 = int(15)
+num3 = int(20)
+
+print(num1 + num2 + num3)
+
+print(num1 - num2 - num3)
+
+print(num1 * num2 * num3)
+
+print(num1 / num2 / num3)
---
+++ level10/homework.py
@@ -0,0 +1,27 @@
+#1)
+num1 = int(10)
+num2 = int(15)
+num3 = int(20)
+
+print(num1 + num2 + num3)
+
+print(num1 - num2 - num3)
+
+print(num1 * num2 * num3)
+
+print(num1 / num2 / num3)
+
+#2)
+
+name = "saba"
+lastname = "germanidze"
+age = 12
+
+print("My name is " + "" + name + " " + "my lastname is" + " " + lastname + " " + "my age is" + " " + str(age))
+
+
+#3)
+
+age = 12
+
+print(age + 36)
