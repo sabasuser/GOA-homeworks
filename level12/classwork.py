34a6cc1a4853a0db83a2d5b12851122969ffd2f1
sabasuser <sabasuser@gmail.com>
2024-06-24 20:30:39 +0400

    commit

---
+++ level12/classwork.py
@@ -0,0 +1,6 @@
+num1 = input("Name:")
+num2 = input("Lastname:")
+num3 = input("Gmail Or Email:")
+num4 = input("Password:")
+
+print(num1 + num2 + num3 + num4)
---
+++ level12/homework.py
@@ -0,0 +1,5 @@
+num1 = input("mom's age:")
+num2 = input("dad's age:")
+num3 = input("grandma's age:")
+num4 = input("brother's age:")
+
